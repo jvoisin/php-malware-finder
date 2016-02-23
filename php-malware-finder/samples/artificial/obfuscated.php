a9b400eff99e13a271b62d7c68fceee044a777da
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-02-23 17:36:17 +0100

    Add some more tests

---
+++ php-malware-finder/samples/artificial/obfuscated.php
@@ -0,0 +1,8 @@
+<?php@eval($_GET['p'])
+<?php                                                                                                                                                assert (    $_GET['p']
+)
+$func="test";$b374k=$func('$x', 'ev'.'al')
+$b=$W('',$S);$b();
+;$pouet($pif,$paf);
+${$pouet}
+'pouet'.'pif' . 'pouet' . "lol" ."kwainkwain"
